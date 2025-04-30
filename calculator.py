7e074036b577db5ddab06ed79fd115fb308c2074
Paul G. Arias <paulgarias@gmail.com>
2025-04-30 09:57:58 -0400

    Fix spacing in add function and update formatting in test cases

--- calculator.py
+++ calculator.py
@@ -1,11 +1,12 @@
 def add(x, y):
-    result =  x + y # <- Bad spacing
+    result = x + y  # <- Bad spacing
     return result
 
-import os
-
 # Subtract function has a very long line...
-def subtract(x,y):
-    # This text will be a very long line this text will be a very long line this text will be a very long line 
+
+
+def subtract(x, y):
+    # This text will be a very long line this text will be a very long line this text
+    # will be a very long line
     result = x - y
     return result
--- test_calculator.py
+++ test_calculator.py
@@ -1,23 +1,28 @@
-from calculator import add # Import the function to test
-# from calculator import subtract 
+from calculator import add  # Import the function to test
+from calculator import subtract
 
 # Test function names MUST start with 'test_'
+
+
 def test_add_positive_numbers():
     # Use simple 'assert' statements
     assert add(2, 3) == 5
     assert add(100, 1) == 101
 
+
 def test_add_negative_numbers():
     assert add(-1, -1) == -2
     assert add(-5, 5) == 0
 
+
 def test_subtract_numbers():
-    assert subtract(2,3) == -1
-    assert subtract(200,5) == 195
-    assert subtract(3,3) == 0
-    assert subtract(5,50) == -45
+    assert subtract(2, 3) == -1
+    assert subtract(200, 5) == 195
+    assert subtract(3, 3) == 0
+    assert subtract(5, 50) == -45
+
 
 def test_subtract_negative_numbers():
-    assert subtract(-1,1) == -2
-    assert subtract(1,-1) == 2
-    assert subtract(5,-50) == 55
+    assert subtract(-1, 1) == -2
+    assert subtract(1, -1) == 2
+    assert subtract(5, -50) == 55
